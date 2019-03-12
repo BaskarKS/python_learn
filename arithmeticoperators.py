1678c148f5f08793d89628ff8afefcace7f45579
BaskarKS <baskarks@gmail.com>
2019-03-12 14:48:55 +0530

    Added new lines for clarity

--- arithmeticoperators.py
+++ arithmeticoperators.py
@@ -2,9 +2,15 @@
 a=10
 b=2
 print(a+b)
+
 print('a - b =>', a-b) #subtract a and b
+
 print('a * b =>', a * b) #product a and b
+
 print("a / b =>", a / b) # float result
+
 print("a % b =>", a % b) # remainder of a and b
+
 print("a // b =>", a // b) #int result if both operands are int
+
 print("a ** b =>", a ** b) #power of a to b
