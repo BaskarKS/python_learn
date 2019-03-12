482cfcb7af22fd2372da1b48bdd15b98b54539c2
BaskarKS <baskarks@gmail.com>
2019-03-12 09:20:21 +0530

    adding comments

--- arithmeticoperators.py
+++ arithmeticoperators.py
@@ -4,7 +4,7 @@ b=2
 print(a+b)
 print('a - b =>', a-b)
 print('a * b =>', a * b)
-print("a / b =>", a / b)
+print("a / b =>", a / b) # float result
 print("a % b =>", a % b)
-print("a // b =>", a // b)
+print("a // b =>", a // b) #int result if both operands are int
 print("a ** b =>", a ** b)
